8a7f39f1e4cd4fa87b41676365e392e1f7b0f410
Nate Hatcher <nhatcher@pkcsecurity.com>
2019-06-28 12:36:28 -0700

    Start on cf yaml parser

--- parsers/cloudformation-yaml.js
+++ parsers/cloudformation-yaml.js
@@ -1,11 +1,35 @@
-const { yamlParse, yamlDump } = require("yaml-cfn");
+const _ = require("lodash");
+const { yamlParse } = require("yaml-cfn");
 const BaseParser = require("./base");
 class CloudFormationYamlParser extends BaseParser {
   async run() {
     const dataString = await this.load(this.path);
-    console.log(dataString);
     const cf = yamlParse(dataString);
-    console.log(cf);
+
+    console.log(JSON.stringify(this.detectTypes(cf), null, 2));
+  }
+
+  detectTypes(cf) {
+    const parsedResources = {};
+    const { Resources } = cf;
+    _.map(Object.keys(Resources), k => {
+      const r = Resources[k];
+      const path = this.parseAwsType(r.Type);
+      path.reduce((o, v) => {
+        if (!o[v]) {
+          o[v] = {};
+        }
+        if (path.indexOf(v) == path.length - 1) {
+          // o[v]["name"] = JSON.stringify(Resources[k]["Properties"]);
+        }
+        return o[v];
+      }, parsedResources);
+    });
+    return parsedResources;
+  }
+
+  parseAwsType(t) {
+    return t.split("::");
   }
 }
 
